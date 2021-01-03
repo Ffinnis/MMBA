3b090f470bd326f0fb1bfab2b804c61597d50ce1
Ffinnis <69709434+Ffinnis@users.noreply.github.com>
2021-01-03 17:28:26 +0900

    some update with @media

--- js/main.js
+++ js/main.js
@@ -19,6 +19,17 @@ $(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
     })
+    window.addEventListener("resize", function(){
+        if (window.innerWidth <=768) {
+            $('.stats').slick('unslick');
+        }
+    });
+    window.addEventListener("resize", function(){
+        if (window.innerWidth <=768) {
+            $('.stats-item__br').toggleClass('stats-item-disable');
+            $('.stats-item-last').toggleClass('stats-item-enable')
+        } 
+    });
 });
 
 function clickBtn() {
@@ -26,13 +37,13 @@ function clickBtn() {
     var $sublist = $('.programm__list-item__sublist');
 
     $btn.click(function(event) {
-        var $target = $(event.currentTarget);
-        var $dropdown = $target.next();
+        var $target = $(event.currentTarget); //отслеживает текущий таргет
+        var $dropdown = $target.next(); //закрывает таргет предыдущий
 
         $btn.not($target).toggleClass('active', false);
         $sublist.not($dropdown).toggleClass('active', false);
-        $target.toggleClass('active', true);
-        $dropdown.toggleClass('active', true);
+        $target.toggleClass('active');
+        $dropdown.toggleClass('active');
     });
 };
 
